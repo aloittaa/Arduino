e5b049975998a8df3976ba8e70bd5afb48697011
Ralf Vogler <ralf.vogler@gmail.com>
2020-03-24 00:57:37 +0100

    add wifi_ota_mqtt, modes via mqtt: off, demo, hex color

--- WS2812B-8/WS2812B-8.ino
+++ WS2812B-8/WS2812B-8.ino
@@ -1,3 +1,7 @@
+#include <MyConfig.h> // credentials, servers, ports
+#define MQTT_TOPIC "lights/WS2812B-8"
+#include "wifi_ota_mqtt.h"
+
 #include <FastLED.h>
 
 // WS2812B 8x1 panel https://www.aliexpress.com/item/32897581470.html
@@ -18,91 +22,120 @@ CRGB leds[NUM_LEDS];
 #define BRIGHTNESS          96
 #define FRAMES_PER_SECOND  120
 
-void setup() {
-  delay(3000); // 3 second delay for recovery
-  
-  // tell FastLED about the LED strip configuration
-  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
-  //FastLED.addLeds<LED_TYPE,DATA_PIN,CLK_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+byte mode = '0';
+CRGB color;
 
-  // set master brightness control
-  FastLED.setBrightness(BRIGHTNESS);
+void mqtt_callback(char* topic, byte* payload, unsigned int length) {
+  Serial.printf("MQTT message on topic %s with payload ", topic);
+  for(int i = 0; i < length; i++){
+    Serial.print((char) *(payload+i));
+  }
+  Serial.println();
+  mode = *payload; // - '0';
+  if (mode == '#') { // convert string hex color to int
+    char c[6]; for(int i = 0; i<6; i++) c[i] = (char) *(payload+1+i); // need to convert (unsigned) byte to (signed) char
+    unsigned long ul = strtoul(c, 0, 16); // would give wrong values for (char*)payload+1 since the content is unsigned
+    Serial.println(ul, HEX);
+    color = ul;
+  }
 }
 
+void setup() {
+  Serial.begin(38400);
+  Serial.println("setup");
+  setup_wifi();
+  setup_OTA();
+  mqtt.setCallback(mqtt_callback);
+  setup_mqtt();
+  
+  delay(2000); // 2 second delay for recovery
+  FastLED.addLeds<LED_TYPE,DATA_PIN,COLOR_ORDER>(leds, NUM_LEDS).setCorrection(TypicalLEDStrip);
+  FastLED.setBrightness(BRIGHTNESS);
+}
 
 // List of patterns to cycle through.  Each is defined as a separate function below.
 typedef void (*SimplePatternList[])();
 SimplePatternList gPatterns = { rainbow, rainbowWithGlitter, confetti, sinelon, juggle, bpm };
 
-uint8_t gCurrentPatternNumber = 0; // Index number of which pattern is current
+uint8_t gCurrentPatternNumber = 0;
 uint8_t gHue = 0; // rotating "base color" used by many of the patterns
-  
+
 void loop()
 {
-  // Call the current pattern function once, updating the 'leds' array
-  gPatterns[gCurrentPatternNumber]();
+  ArduinoOTA.handle();
+  mqtt.loop();
+  switch (mode) {
+    case '0':
+      // FastLED.clear();
+      fill(CRGB::Black);
+      break;
+    case '#':
+      fill(color);
+      break;
+    default:
+      // Call the current pattern function once, updating the 'leds' array
+      gPatterns[gCurrentPatternNumber]();
+      // send the 'leds' array out to the actual LED strip
+      FastLED.show();  
+      // insert a delay to keep the framerate modest
+      FastLED.delay(1000/FRAMES_PER_SECOND); 
+      // do some periodic updates
+      EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
+      EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+  }
+}
 
-  // send the 'leds' array out to the actual LED strip
-  FastLED.show();  
-  // insert a delay to keep the framerate modest
-  FastLED.delay(1000/FRAMES_PER_SECOND); 
-
-  // do some periodic updates
-  EVERY_N_MILLISECONDS( 20 ) { gHue++; } // slowly cycle the "base color" through the rainbow
-  EVERY_N_SECONDS( 10 ) { nextPattern(); } // change patterns periodically
+void fill(CRGB color) {
+  for(int i = 0; i < NUM_LEDS; i++) {
+    leds[i] = color;
+  }
+  FastLED.show();
 }
 
 #define ARRAY_SIZE(A) (sizeof(A) / sizeof((A)[0]))
 
-void nextPattern()
-{
+void nextPattern() {
   // add one to the current pattern number, and wrap around at the end
   gCurrentPatternNumber = (gCurrentPatternNumber + 1) % ARRAY_SIZE( gPatterns);
 }
 
-void rainbow() 
-{
+void rainbow() {
   // FastLED's built-in rainbow generator
   fill_rainbow( leds, NUM_LEDS, gHue, 7);
 }
 
-void rainbowWithGlitter() 
-{
+void rainbowWithGlitter() {
   // built-in FastLED rainbow, plus some random sparkly glitter
   rainbow();
   addGlitter(80);
 }
 
-void addGlitter( fract8 chanceOfGlitter) 
-{
+void addGlitter( fract8 chanceOfGlitter) {
   if( random8() < chanceOfGlitter) {
     leds[ random16(NUM_LEDS) ] += CRGB::White;
   }
 }
 
-void confetti() 
-{
+void confetti() {
   // random colored speckles that blink in and fade smoothly
   fadeToBlackBy( leds, NUM_LEDS, 10);
   int pos = random16(NUM_LEDS);
   leds[pos] += CHSV( gHue + random8(64), 200, 255);
 }
 
-void sinelon()
-{
+void sinelon() {
   // a colored dot sweeping back and forth, with fading trails
   fadeToBlackBy( leds, NUM_LEDS, 20);
   int pos = beatsin16( 13, 0, NUM_LEDS-1 );
   leds[pos] += CHSV( gHue, 255, 192);
 }
 
-void bpm()
-{
+void bpm() {
   // colored stripes pulsing at a defined Beats-Per-Minute (BPM)
   uint8_t BeatsPerMinute = 62;
   CRGBPalette16 palette = PartyColors_p;
   uint8_t beat = beatsin8( BeatsPerMinute, 64, 255);
-  for( int i = 0; i < NUM_LEDS; i++) { //9948
+  for(int i = 0; i < NUM_LEDS; i++) { //9948
     leds[i] = ColorFromPalette(palette, gHue+(i*2), beat-gHue+(i*10));
   }
 }
@@ -111,7 +144,7 @@ void juggle() {
   // eight colored dots, weaving in and out of sync with each other
   fadeToBlackBy( leds, NUM_LEDS, 20);
   byte dothue = 0;
-  for( int i = 0; i < 8; i++) {
+  for(int i = 0; i < 8; i++) {
     leds[beatsin16( i+7, 0, NUM_LEDS-1 )] |= CHSV(dothue, 200, 255);
     dothue += 32;
   }
---
+++ WS2812B-8/wifi_ota_mqtt.h
@@ -0,0 +1,106 @@
+// WiFi
+#ifdef ESP32
+  #include <WiFi.h>
+#else
+  #include <ESP8266WiFi.h>
+#endif
+WiFiClient wifi;
+
+void setup_wifi() {
+  delay(5);
+  Serial.printf("Connecting to AP %s", WIFI_SSID);
+  const unsigned long start_time = millis();
+  WiFi.mode(WIFI_STA);
+  WiFi.begin(WIFI_SSID, WIFI_PASS);
+  for (int i = 0; WiFi.waitForConnectResult() != WL_CONNECTED && i < 10; i++) {
+    #ifdef ESP32
+      WiFi.begin(WIFI_SSID, WIFI_PASS); // for ESP32 also had to be moved inside the loop, otherwise only worked on every second boot, https://github.com/espressif/arduino-esp32/issues/2501#issuecomment-548484502
+    #endif
+    delay(200);
+    Serial.print(".");
+  }
+  const float connect_time = (millis() - start_time) / 1000.;
+  Serial.println();
+  if (WiFi.waitForConnectResult() != WL_CONNECTED) {
+    Serial.printf("Failed to connect to Wifi in %.3f seconds. Going to restart!", connect_time);
+    ESP.restart();
+  }
+  Serial.printf("Connected in %.3f seconds. IP address: ", connect_time);
+  Serial.println(WiFi.localIP());
+}
+
+
+// OTA update
+#ifdef ESP32
+  #include <ESPmDNS.h>
+#else
+  #include <ESP8266mDNS.h>
+#endif
+#include <WiFiUdp.h>
+#include <ArduinoOTA.h>
+
+void setup_OTA() {
+  ArduinoOTA.onStart([]() {
+    String type;
+    if (ArduinoOTA.getCommand() == U_FLASH) {
+      type = "sketch";
+    } else { // U_SPIFFS
+      type = "filesystem";
+    }
+    // NOTE: if updating SPIFFS this would be the place to unmount SPIFFS using SPIFFS.end()
+    Serial.println("OTA: Start updating " + type);
+    // OLED_stat("OTA start");
+  });
+  ArduinoOTA.onEnd([]() {
+    Serial.println("\nOTA: Done");
+    // OLED.clearDisplay(); OLED.display();
+  });
+  ArduinoOTA.onProgress([](unsigned int progress, unsigned int total) {
+    Serial.printf("OTA: Progress: %u%%\r", (progress / (total / 100)));
+    // OLED_stat("OTA: %u%%", (progress / (total / 100)));
+  });
+  ArduinoOTA.onError([](ota_error_t error) {
+    Serial.printf("OTA Error[%u]: ", error);
+    // OLED_stat("OTA fail: %u\n", error);
+    if (error == OTA_AUTH_ERROR) {
+      Serial.println("Auth Failed");
+    } else if (error == OTA_BEGIN_ERROR) {
+      Serial.println("Begin Failed");
+    } else if (error == OTA_CONNECT_ERROR) {
+      Serial.println("Connect Failed");
+    } else if (error == OTA_RECEIVE_ERROR) {
+      Serial.println("Receive Failed");
+    } else if (error == OTA_END_ERROR) {
+      Serial.println("End Failed");
+    }
+  });
+  ArduinoOTA.begin();
+}
+
+
+// MQTT
+#include <PubSubClient.h>
+PubSubClient mqtt(wifi);
+
+void setup_mqtt() {
+  mqtt.setServer(MQTT_SERVER, MQTT_PORT);
+  // mqtt.setCallback(mqtt_callback);
+  randomSeed(micros());
+  while (!mqtt.connected()) {
+    Serial.print("Attempting MQTT connection... ");
+    String clientId = "lights-WS2812B-8-" + String(random(0xffff), HEX);
+    if (mqtt.connect(clientId.c_str())) {
+      Serial.printf("connected as %s to mqtt://%s\n", clientId.c_str(), MQTT_SERVER);
+      while(!mqtt.subscribe(MQTT_TOPIC)) Serial.print(".");
+      Serial.printf("subscribed to topic %s\n", MQTT_TOPIC);
+    } else {
+      Serial.printf("failed, rc=%d. retry in 1s.\n", mqtt.state());
+      delay(1000);
+    }
+  }
+}
+
+
+// JSON
+char buf[200];
+#define json(s, ...) (sprintf(buf, "{ " s " }", __VA_ARGS__), buf)
